a33016f42c01694271385bd2534a1eda2c68f221
w001ex <kita120602@gmail.com>
2024-02-26 00:53:21 +0900

    240226

--- docs/Src/main.js
+++ docs/Src/main.js
@@ -3,18 +3,21 @@ const ctx = canvas.getContext("2d");//2次元描画
 var img = new Image();
 img.src = 'Src/sea.jpg'; 
 var img2 = new Image();
-img2.src = 'Src/fish.png'; 
+img2.src = 'Src/fish.png';
+var img3 = new Image();
+img3.src = 'Src/fish2.png';
+
 
 window.onload = function() {
     const x = canvas.width;
     const y = canvas.height;
-    const N = 200;
-    const size = 50;
-    const w1 = 2;
-    const w2 = 2;
-    const w3 = 2;
-    const dt = 0.005;
-    const c = -0.75;//閾値
+    const N = 50;
+    const size = 250;
+    const w1 = 0.1;
+    const w2 = 0.1;
+    const w3 = 0.1;
+    const dt = 0.0075;
+    const c = 0;//閾値() c%がランダムに動く
     const r = size;//判定半径
     let pos_X = Array(N);
     let pos_Y = Array(N);
@@ -25,6 +28,7 @@ window.onload = function() {
     let vel_X1 = Array(N);
     let vel_Y1 = Array(N);
     let theta = Array(N);
+    let tag = Array(N);
     let step = 0;
     //↑定数と変数の定義
     if (canvas.getContext) {
@@ -39,6 +43,11 @@ window.onload = function() {
             var vel_Y_i = Math.random();
             vel_X[i] = 500*(vel_X_i - 0.5);
             vel_Y[i] = 500*(vel_Y_i - 0.5);
+            if(i % 2 == 0){
+                tag[i] = 0;
+            }else{
+                tag[i] = 1;
+            }
           }//初期位置と初期速度の定義
           for (let i = 0; i < N; i++){
             pos_X1[i] = pos_X[i];
@@ -66,10 +75,10 @@ window.onload = function() {
                 vel0 = w1*cohesion(i)[0] + w2*separation(i)[0] + w3*alignment(i)[0]; 
                 vel1 = w1*cohesion(i)[1] + w2*separation(i)[1] + w3*alignment(i)[1];
                 let rand = 0;
-                rand = 2*Math.random() -1;
-                if(rand > c){
-                    vel0 = 1000*(Math.random()-0.5);
-                    vel1 = 1000*(Math.random()-0.5);
+                rand = 100*Math.random();
+                if(rand < c){
+                    vel0 += 1000*(Math.random()-0.5);
+                    vel1 += 1000*(Math.random()-0.5);
                 }
                 vel_X1[i] += vel0*dt;
                 vel_Y1[i] += vel1*dt;
@@ -85,11 +94,16 @@ window.onload = function() {
                 ctx.translate(pos_X1[i], pos_Y1[i]);
                 ctx.rotate(theta[i] - 0.5*Math.PI);
                 console.log(theta[i]);
-                ctx.drawImage(img2,-size/2,-size/2,size,size);
+                if(tag[i] == 0){
+                    ctx.drawImage(img2,-size/2,-size/2,0.5*size,0.5*size);
+                }else{
+                    ctx.drawImage(img3,-size/2,-size/2,0.5*size,0.5*size);
+                }
                 ctx.restore();
             }
             ctx.fillText("Hello Aquarium!!", 0, 40);
             ctx.fillText("illustration:Reika Sano", x - 250, y - 40);
+            ctx.fillText("illustration:Riiko Fujiwara", x - 250, y - 20);
             for (let i = 0; i < N; i++){
                 pos_X[i] = pos_X1[i];
                 pos_Y[i] = pos_Y1[i];
@@ -122,11 +136,16 @@ window.onload = function() {
             acc1[1] = 0;
             let num = 0;
             for (let i = 0; i < N; i++){
-                if((pos_X[j]-pos_X[i])**2 + (pos_Y[j]-pos_Y[i])**2 < r**2 && i != j){//距離判定
+                if((pos_X[j]-pos_X[i])**2 + (pos_Y[j]-pos_Y[i])**2 < r**2 && i != j && tag[i] == tag[j]){//距離判定
                     num ++;
                     acc1[0] += pos_X[i];
                     acc1[1] += pos_Y[i];
                 }
+                // if((pos_X[j]-pos_X[i])**2 + (pos_Y[j]-pos_Y[i])**2 < r**2 && i != j && tag[i] != tag[j]){
+                //     num ++;
+                //     acc1[0] -= pos_X[i];
+                //     acc1[1] -= pos_Y[i];
+                // }
             }
             if(num != 0){
                 acc1[0] /= num;
@@ -143,11 +162,16 @@ window.onload = function() {
                 acc2[1] = 0;
                 let num = 0;
                 for (let i = 0; i < N; i++){
-                    if((pos_X[j]-pos_X[i])**2 + (pos_Y[j]-pos_Y[i])**2 < r**2 && i != j){//距離判定
+                    if((pos_X[j]-pos_X[i])**2 + (pos_Y[j]-pos_Y[i])**2 < r**2 && i != j && tag[i] == tag[j]){//距離判定
                         num ++;
-                        acc2[0] += (pos_X[j] - pos_X[i])/((pos_X[j]-pos_X[i])**2 + (pos_Y[j]-pos_Y[i])**2 + 1);
-                        acc2[1] += (pos_Y[j] - pos_Y[i])/((pos_X[j]-pos_X[i])**2 + (pos_Y[j]-pos_Y[i])**2 + 1);
+                        acc2[0] += (pos_X[j] - pos_X[i])/((pos_X[j]-pos_X[i])**2 + (pos_Y[j]-pos_Y[i])**2)**2;
+                        acc2[1] += (pos_Y[j] - pos_Y[i])/((pos_X[j]-pos_X[i])**2 + (pos_Y[j]-pos_Y[i])**2)**2;
                     }
+                    // if((pos_X[j]-pos_X[i])**2 + (pos_Y[j]-pos_Y[i])**2 < r**2 && i != j && tag[i] != tag[j]){//距離判定
+                    //     num ++;
+                    //     acc2[0] -= (pos_X[j] - pos_X[i])/((pos_X[j]-pos_X[i])**2 + (pos_Y[j]-pos_Y[i])**2)**2;
+                    //     acc2[1] -= (pos_Y[j] - pos_Y[i])/((pos_X[j]-pos_X[i])**2 + (pos_Y[j]-pos_Y[i])**2)**2;
+                    // }
                 }
                 if(num != 0){
                     acc2[0] /= num;
@@ -162,11 +186,16 @@ window.onload = function() {
             acc3[1] = 0;
             let num =0;
             for (let i = 0; i < N; i++){
-                if((pos_X[j]-pos_X[i])**2 + (pos_Y[j]-pos_Y[i])**2 < r**2 && i != j){//距離判定
+                if((pos_X[j]-pos_X[i])**2 + (pos_Y[j]-pos_Y[i])**2 < r**2 && i != j && tag[i] == tag[j]){//距離判定
                     num ++;
                     acc3[0] += vel_X[i];
                     acc3[1] += vel_Y[i];
                 }
+                // if((pos_X[j]-pos_X[i])**2 + (pos_Y[j]-pos_Y[i])**2 < r**2 && i != j && tag[i] != tag[j]){//距離判定
+                //     num ++;
+                //     acc3[0] -= vel_X[i];
+                //     acc3[1] -= vel_Y[i];
+                // }
             }
             if(num != 0){
                 acc3[0] /= num;
