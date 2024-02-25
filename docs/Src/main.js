cfd3f85eea36f8344ef741391868fa9b8512c603
w001ex <kita120602@gmail.com>
2024-02-25 18:40:10 +0900

    240225

--- docs/Src/main.js
+++ docs/Src/main.js
@@ -2,18 +2,20 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");//2次元描画
 var img = new Image();
 img.src = 'Src/sea.jpg'; 
+var img2 = new Image();
+img2.src = 'Src/fish.png'; 
 console.log("check");
 
 window.onload = function() {
     const x = canvas.width;
     const y = canvas.height;
-    const N = 50;
-    const size = 5;
-    const w1 = 1;
-    const w2 = 1;
-    const w3 = 1;
+    const N = 200;
+    const size = 50;
+    const w1 = 2;
+    const w2 = 2;
+    const w3 = 2;
     const dt = 0.005;
-    const c = 0;//閾値
+    const c = -0.75;//閾値
     const r = size;//判定半径
     let pos_X = Array(N);
     let pos_Y = Array(N);
@@ -23,6 +25,7 @@ window.onload = function() {
     let pos_Y1 = Array(N);
     let vel_X1 = Array(N);
     let vel_Y1 = Array(N);
+    let theta = Array(N);
     let step = 0;
     //↑定数と変数の定義
     if (canvas.getContext) {
@@ -44,51 +47,21 @@ window.onload = function() {
             vel_X1[i] = vel_X[i];
             vel_Y1[i] = vel_Y[i];
             }
-          //ctx.strokeRect(0,0,x,y);//キャンバスを示す
-        for (let i = 0; i < N; i++){
-            ctx.strokeRect(pos_X1[i],pos_Y1[i],size,size);//各座標に描画
-        }
         //↑初期化
         function translate() {
             step ++;
-            //以下外乱
-            // if(step%1000 == 0){
-            //     for (let i = 0; i < N; i++){
-            //         vel_X[i] = -vel_X[i];
-            //         vel_Y[i] = -vel_Y[i];
-            //         vel_X1[i] = -vel_X1[i];
-            //         vel_Y1[i] = -vel_Y1[i];
-            //     }
-            // }
-            // if(step%1000 == 333){
-            //     for (let i = 0; i < N; i++){
-            //         vel_X[i] = -vel_Y[i];
-            //         vel_Y[i] = vel_X[i];
-            //         vel_X1[i] = -vel_Y1[i];
-            //         vel_Y1[i] = vel_X[i];
-            //     }
-            // }
-            // if(step%1000 == 667){
-            //     for (let i = 0; i < N; i++){
-            //         vel_X[i] = vel_Y[i];
-            //         vel_Y[i] = -vel_X[i];
-            //         vel_X1[i] = vel_Y1[i];
-            //         vel_Y1[i] = -vel_X1[i];
-            //     }
-            // }   
-            //外乱終わり
             for (let i = 0; i < N; i++){
-                if(pos_X1[i] <= 0 && vel_X1[i] <= 0){
-                    pos_X1[i] += x;
+                if(pos_X1[i] + size<= 0 && vel_X1[i] <= 0){
+                    pos_X1[i] += x + 2*size;
                 }
-                if(pos_X1[i] + size >= x && vel_X1[i] >= 0){
-                    pos_X1[i] -= x;
+                if(pos_X1[i] >= x + size && vel_X1[i] >= 0){
+                    pos_X1[i] -= x + 2*size;
                 }
-                if(pos_Y1[i] <= 0 && vel_Y1[i] <= 0){
-                    pos_Y1[i] += y;
+                if(pos_Y1[i] + size <= 0 && vel_Y1[i] <= 0){
+                    pos_Y1[i] += y + 2*size;
                 }
-                if(pos_Y1[i] + size >= y && vel_Y1[i] >= 0){
-                    pos_Y1[i] -= y;
+                if(pos_Y1[i]  >= y + size && vel_Y1[i] >= 0){
+                    pos_Y1[i] -= y + 2*size;
                 }
 
                 vel0 = w1*cohesion(i)[0] + w2*separation(i)[0] + w3*alignment(i)[0]; 
@@ -101,18 +74,21 @@ window.onload = function() {
                 }
                 vel_X1[i] += vel0*dt;
                 vel_Y1[i] += vel1*dt;
+                theta[i] = getTheta(vel_X1[i], vel_Y1[i]);
                 pos_X1[i] += vel_X1[i]*dt;
                 pos_Y1[i] += vel_Y1[i]*dt;    
             }
-            ctx.clearRect(0,0,canvas.width, canvas.height);//リセット
-            ctx.beginPath();
-            // ctx.strokeRect(0,0,x,y);//輪郭線の四角形
             ctx.drawImage(img, 0, 0, x, y);
             ctx.fillStyle = "black";
             ctx.font = "20px sans-serif";
             ctx.fillText("Hellow Aquarium!!", 0, 40);
             for (let i = 0; i < N; i++){
-                ctx.strokeRect(pos_X1[i],pos_Y1[i],size,size);//各座標に描画
+                ctx.save();
+                ctx.translate(pos_X1[i], pos_Y1[i]);
+                ctx.rotate(theta[i] - 0.5*Math.PI);
+                console.log(theta[i]);
+                ctx.drawImage(img2,-size/2,-size/2,size,size);
+                ctx.restore();
             }
             for (let i = 0; i < N; i++){
                 pos_X[i] = pos_X1[i];
@@ -124,7 +100,22 @@ window.onload = function() {
           }
           
         translate();
-          
+        function getTheta(vx,vy){
+            let cos = 0;
+            let theta = 0;
+            if(vx**2 + vy**2 == 0){
+                theta = 0;
+            }else{
+                cos = vx / Math.sqrt(vx**2 + vy**2)
+            }
+            if(vy >= 0){
+                theta = Math.acos(cos); 
+            }else{
+                theta = -Math.acos(cos);
+            }
+            return theta;
+        }
+
         function cohesion(j){
             let acc1 = Array(2);
             acc1[0] = 0;
